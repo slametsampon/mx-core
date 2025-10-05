80a02c6fe043f887c4441b52c36b0540c37cdeb9
slametsampon <slamet@pon.co.id>
2025-10-05 18:26:02 +0700

    init setup

--- next.config.js
+++ next.config.js
@@ -1,9 +1,15 @@
-const { withContentlayer } = require("next-contentlayer");
+// next.config.js
+
+const { withContentlayer } = require('next-contentlayer');
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
 };
 
 module.exports = withContentlayer(nextConfig);
