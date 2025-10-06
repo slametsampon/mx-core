340c2d95a1547ef88c773caa091fb35a395c370e
Slamet <slamet@pon.co.id>
2025-10-06 08:08:39 +0700

    setup advance fitur

--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -1,9 +1,8 @@
+// data/headerNavLinks.ts
+
 const headerNavLinks = [
   { href: '/', title: 'Home' },
-  { href: '/ai-chat', title: 'AI Chat' },
-  { href: '/activities', title: 'Activities' },
   { href: '/blog', title: 'Blog' },
-  { href: '/projects', title: 'Projects' },
   { href: '/about', title: 'About' },
 ]
 
