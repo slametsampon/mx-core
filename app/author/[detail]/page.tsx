a45c018123f7e21ca4693f1e6d19f59202f10ced
Slamet <slamet@pon.co.id>
2025-10-07 13:27:20 +0700

    update detail author

--- app/author/[detail]/page.tsx
+++ app/author/[detail]/page.tsx
@@ -1,14 +1,20 @@
-import { slug } from 'github-slugger'
-import { allCoreContent, sortPosts } from 'pliny/utils/contentlayer'
-import siteMetadata from '@/data/siteMetadata'
-import ListLayout from '@/layouts/ListLayoutWithTags'
-import { allAuthors, allBlogs } from 'contentlayer/generated'
-import authorData from 'app/author-data.json'
-import { genPageMetadata } from 'app/seo'
-import CardAuthor from '@/components/CardAuthor'
+// app/author/[detail]/page.tsx
 
-export async function generateMetadata({ params }: { params: { detail: string } }) {
-  const detail = decodeURI(params.detail)
+import { slug } from 'github-slugger';
+import { allCoreContent, sortPosts } from 'pliny/utils/contentlayer';
+import siteMetadata from '@/data/siteMetadata';
+import ListLayout from '@/layouts/ListLayoutWithTags';
+import { allAuthors, allBlogs } from 'contentlayer/generated';
+import authorData from 'app/author-data.json';
+import { genPageMetadata } from 'app/seo';
+import CardAuthor from '@/components/CardAuthor';
+
+export async function generateMetadata({
+  params,
+}: {
+  params: { detail: string };
+}) {
+  const detail = decodeURI(params.detail);
   return genPageMetadata({
     title: detail,
     description: `${siteMetadata.title} ${detail} tagged content`,
@@ -18,33 +24,36 @@ export async function generateMetadata({ params }: { params: { detail: string } 
         'application/rss+xml': `${siteMetadata.siteUrl}/author/${detail}/feed.xml`,
       },
     },
-  })
+  });
 }
 
 export const generateStaticParams = async () => {
-  const authorCounts = authorData as Record<string, number>
-  const authorKeys = Object.keys(authorCounts)
+  const authorCounts = authorData as Record<string, number>;
+  const authorKeys = Object.keys(authorCounts);
   const paths = authorKeys.map((author) => ({
     author: author,
-  }))
-  return paths
-}
+  }));
+  return paths;
+};
 
 export default function AuthorPage({ params }: { params: { detail: string } }) {
-  const detail = decodeURI(params.detail)
+  const detail = decodeURI(params.detail);
   // Capitalize first letter and convert space to dash
-  const title = detail[0].toUpperCase() + detail.split(' ').join('-').slice(1)
+  const title = detail[0].toUpperCase() + detail.split(' ').join('-').slice(1);
   const filteredPosts = allCoreContent(
     sortPosts(
-      allBlogs.filter((post) => post.authors && post.authors.map((t) => slug(t)).includes(detail))
+      allBlogs.filter(
+        (post) =>
+          post.authors && post.authors.map((t) => slug(t)).includes(detail)
+      )
     )
-  )
+  );
   //get author
-  const authorResult = allAuthors.find((p) => p.slug === detail)
+  const authorResult = allAuthors.find((p) => p.slug === detail);
   return (
     <>
       <CardAuthor author={authorResult} />
       <ListLayout posts={filteredPosts} title={title} />
     </>
-  )
+  );
 }
