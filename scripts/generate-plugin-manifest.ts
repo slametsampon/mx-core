4206bdcbd322e83532841ecceb90e8f32c527b98
Slamet <slamet@pon.co.id>
2025-11-21 15:15:06 +0700

    fix import { loadPlugins } from '../packages/core/src';

--- scripts/generate-plugin-manifest.ts
+++ scripts/generate-plugin-manifest.ts
@@ -2,7 +2,7 @@
 
 import fs from 'fs';
 import path from 'path';
-import { loadPlugins, PluginMeta } from '@mx-core/core';
+import { loadPlugins, PluginMeta } from '../packages/core/src';
 
 async function main() {
   const pluginsDir = 'plugins';
