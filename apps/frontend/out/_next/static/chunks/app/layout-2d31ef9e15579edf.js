3f64e04933d4c6b34ab9ff8053479abbb25afa0c
Slamet <slamet@pon.co.id>
2025-11-21 07:59:22 +0700

    build:all success

--- apps/frontend/out/_next/static/chunks/app/layout-2d31ef9e15579edf.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[185],{5751:function(){}},function(n){n.O(0,[744,9,560],function(){return n(n.s=5751)}),_N_E=n.O()}]);
--- apps/frontend/out/_next/static/chunks/app/page-8e9fb5a204522f53.js
+++
@@ -1,9 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[931],{3318:function(e,r,t){Promise.resolve().then(t.bind(t,1703))},1703:function(e,r,t){"use strict";t.r(r),t.d(r,{default:function(){return o}});var n=t(4618),s=t(7024);function o(){let[e,r]=(0,s.useState)([]);return(0,s.useEffect)(()=>{fetch("/plugin-manifest.json").then(e=>e.json()).then(r)},[]),(0,n.jsxs)("div",{className:"p-6",children:[(0,n.jsx)("h1",{className:"mb-4 text-2xl font-bold",children:"Frontend Home"}),(0,n.jsx)("div",{className:"space-y-2",children:e.map(e=>(0,n.jsxs)("a",{href:e.basePath,className:"block rounded border p-4 hover:bg-gray-100",children:[(0,n.jsx)("h2",{className:"text-xl font-semibold",children:e.name}),(0,n.jsx)("p",{className:"text-sm text-gray-600",children:e.description})]},e.name))})]})}},1432:function(e,r,t){"use strict";/**
- * @license React
- * react-jsx-runtime.production.min.js
- *
- * Copyright (c) Meta Platforms, Inc. and affiliates.
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE file in the root directory of this source tree.
- */var n=t(7024),s=Symbol.for("react.element"),o=(Symbol.for("react.fragment"),Object.prototype.hasOwnProperty),a=n.__SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED.ReactCurrentOwner,c={key:!0,ref:!0,__self:!0,__source:!0};function l(e,r,t){var n,l={},i=null,f=null;for(n in void 0!==t&&(i=""+t),void 0!==r.key&&(i=""+r.key),void 0!==r.ref&&(f=r.ref),r)o.call(r,n)&&!c.hasOwnProperty(n)&&(l[n]=r[n]);if(e&&e.defaultProps)for(n in r=e.defaultProps)void 0===l[n]&&(l[n]=r[n]);return{$$typeof:s,type:e,key:i,ref:f,props:l,_owner:a.current}}r.jsx=l,r.jsxs=l},4618:function(e,r,t){"use strict";e.exports=t(1432)}},function(e){e.O(0,[744,9,560],function(){return e(e.s=3318)}),_N_E=e.O()}]);
