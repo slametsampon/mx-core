34a3df3d3c8467ea8ffa367e9622683c46aad367
slametsampon <slamet@pon.co.id>
2025-12-02 20:46:45 +0700

    update plugins/mx-core-metric package.json

--- plugins/mx-core-metric/next.config.js
+++ plugins/mx-core-metric/next.config.js
@@ -7,9 +7,6 @@ const nextConfig = {
   // Opsional: buat URL jadi pakai slash di akhir (SEO friendly)
   trailingSlash: true,
 
-  // ⚠️ Tambahkan jika kamu ingin deploy plugin ini di subfolder
-  // basePath: '/plugins/metric', // contoh jika plugin ini dipasang di frontend/docs
-
   // Aktifkan source map untuk debugging di Vercel (opsional)
   productionBrowserSourceMaps: true,
 };
